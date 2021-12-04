ce917b051a6d35c59eb1be23629184c477e729f9
yemza <elbouazzaoui729@gmail.com>
2021-12-04 22:33:31 +0100

    add athentication and Login Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ const routes: Routes = [
    
   {
     path : '',
-    redirectTo :'/home',
+    redirectTo :'/login',
     pathMatch :'full'
   },
 
- 
   {
     path : '',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+ 
+  {
+    path : 'stickers',
     loadChildren: () => import('./application/application.module').then(m => m.ApplicationModule)
   },
 

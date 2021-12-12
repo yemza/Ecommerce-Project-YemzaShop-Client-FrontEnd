99c02eaf848d3aeb9559dc8b0c3f8b1f4247f1a0
yemza <elbouazzaoui729@gmail.com>
2021-12-12 19:28:48 +0100

    Get Token From Backen

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/_core/services/authentication.service';
+import { ReactiveFormsService } from 'src/app/_core/services/reactive-forms.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginFormGroup!: FormGroup ;
+  private subs = new Subscription();
+
+
+  constructor(private fb : FormBuilder , 
+              private  quthenticationService: AuthenticationService ,
+              private  reactiveFormsService: ReactiveFormsService) { }
 
   ngOnInit(): void {
+    /**Create the form */
+    this.createLoginFormGroup()
   }
+
+
+  /**
+   * Create Loging Form Group
+   * To get User Data
+   */
+  createLoginFormGroup(){
+    this.loginFormGroup = this.fb.group({
+      username : [null, Validators.required],
+      password : [null, Validators.required]
+    })
+
+  }
+
+  /**
+   * Submit Request Login User
+   */
+   onSubmit(){
+   console.log(this.loginFormGroup?.get('username')?.invalid)
+   if(this.loginFormGroup.valid){
+     this.subs.add(
+      this.quthenticationService.Login(this.loginFormGroup?.value,  {skip_token:'true'}
+        ).subscribe(
+        res =>{
+          console.log(res)
+        }
+      ))
+   }else{
+     this.reactiveFormsService.validateAllFormFields(this.loginFormGroup)
+   }
+  }
+
+  /**
+   * On Destroy
+   * To destroy All Services
+   */
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+    }
 
 }
